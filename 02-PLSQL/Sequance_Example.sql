863ca12c6faa248f7130b2ee761f4c0bc2eaf2e0
m-parhizkar <mohammad.parhizkar@gmail.com>
2019-04-04 07:24:50 +0200

    Update Sequance_Example.sql

--- 02-PLSQL/Sequance_Example.sql
+++ 02-PLSQL/Sequance_Example.sql
@@ -1,5 +1,9 @@
 /*
-A sequence is a set of int numbers in both ascending or descending order, which is generated and approved by some database systems to deliver unique values on demand. The sequences provide an easy way to generate a unique primary key for each row (tuple) in a database.
+A sequence is a set of int numbers in both ascending 
+or descending order, which is generated and approved 
+by some database systems to deliver unique values on 
+demand. The sequences provide an easy way to generate 
+a unique primary key for each row (tuple) in a database.
 */
 
 
